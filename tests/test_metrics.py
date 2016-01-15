e0acba18b7a61f3b9ba24470ba0bee3558ba173b
Charles S. Givre <cgivre@users.noreply.github.com>
2016-01-15 13:19:32 -0500

    Update test_metrics.py
    Used unittests,

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,14 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pydrill_setup
+----------------------------------
+
+Tests for `pydrill` metrics function.
+Related to setup of :class:~`pydrill.PyDrill`.
+"""
+
+import unittest
+
 from pydrill.client import PyDrill
-drill = PyDrill(host='localhost', port=8047)
 
-if not drill.is_active():
-    raise ImproperlyConfigured('Please run Drill first')
 
-metrics = drill.metrics()
-for data in drill.metrics():
-    print( data )
+class TestPydrill(unittest.TestCase):
+    def setUp(self):
+        self.drill = PyDrill(host='localhost', port=8047)
 
-print(metrics.keys)
-print(metrics.data)
-print(metrics.values)
-print(drill.metrics().max_direct_memory)
+    def test_transport_host(self):
+        assert self.drill.transport.host == 'localhost'
+
+    def test_transport_port(self):
+        assert self.drill.transport.port == 8047
+
+    # TODO: create more tests checking other params.
+
+    #The values of the metrics will differ from machine to machine, so basically
+    #just testing that it works.
+    def test_metrics(self):
+        metrics = self.drill.metrics()
+        assert len( metrics.data ) == 6
+        assert metrics.number_of_drill_bits >= 1
+        assert isinstance( metrics.max_direct_memory, int )
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
+
+
