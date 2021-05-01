538d4dfc35e1face0035d17e6811aacd4d2034eb
Wojtek Nowak <mail@pythonic.ninja>
2021-05-01 13:31:47 +0200

    fix qa issues, and tox envlist,

--- pydrill/compat.py
+++ pydrill/compat.py
@@ -5,11 +5,11 @@ import sys
 PY2 = sys.version_info[0] == 2
 
 if PY2:
-    string_types = basestring,
+    string_types = basestring, # noqa
     from urllib import quote_plus, urlencode  # noqa
     from urlparse import urlparse  # noqa
-    from itertools import imap as map
+    from itertools import imap as map # noqa
 else:
-    string_types = str, bytes
+    string_types = str, bytes # noqa
     from urllib.parse import quote_plus, urlencode, urlparse  # noqa
-    map = map
+    map = map # noqa
