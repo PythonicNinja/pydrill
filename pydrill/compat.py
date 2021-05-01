567633eb512fcd98623318cb06d3a4d538e3f000
Wojtek Nowak <mail@pythonic.ninja>
2021-05-01 13:42:59 +0200

    fix qa in compat.py

--- pydrill/compat.py
+++ pydrill/compat.py
@@ -5,11 +5,11 @@ import sys
 PY2 = sys.version_info[0] == 2
 
 if PY2:
-    string_types = basestring, # noqa
+    string_types = basestring,  # noqa
     from urllib import quote_plus, urlencode  # noqa
     from urlparse import urlparse  # noqa
-    from itertools import imap as map # noqa
+    from itertools import imap as map  # noqa
 else:
-    string_types = str, bytes # noqa
+    string_types = str, bytes  # noqa
     from urllib.parse import quote_plus, urlencode, urlparse  # noqa
-    map = map # noqa
+    map = map  # noqa
